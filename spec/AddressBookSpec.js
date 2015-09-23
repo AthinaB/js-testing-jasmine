e91fac319f8c5254569163f83908a676c6141f94
Athina Bekakou <athinabek@grnet.gr>
2015-09-24 00:30:35 +0300

    Add test for async address book

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -18,3 +18,22 @@ describe('Address Book', function() {
 		expect(addressB.getContact(0)).not.toBeDefined();
 	});
 });
+
+describe('Async Address Book', function() {
+	var addressB = new addressBook();
+
+	/*
+	 * the func getAsyncContacts should take as param
+	 * a callback func and invokes it gets a response
+	*/
+	beforeEach(function(done) {
+		addressB.getAsyncContacts(function() {
+			done();
+		});
+	});
+
+	it('should get the contacts async', function(done) {
+		expect(addressB.asyncCompleted).toBe(true);
+		done();
+	});
+});
