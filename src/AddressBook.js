8890eefee0a27937090d8f8fbddb27827340a867
Athina Bekakou <athinabek@grnet.gr>
2015-09-26 18:49:15 +0300

    Write code with async function to pass the test

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -9,6 +9,17 @@ function addressBook() {
 		},
 		deleteContact: function(i) {
 			return contacts = contacts.splice(1, 1);
+		},
+		asyncCompleted: false,
+		// cb is used in order to invoke done of Jasmine
+		getAsyncContacts: function(cb) {
+			var self = this;
+			setTimeout(function() {
+				self.asyncCompleted = true;
+				if (cb) {
+					return cb();
+				}
+			}, 300);
 		}
 	}
 };
